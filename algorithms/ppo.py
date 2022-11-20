ea70f890d8392e1491202b2492dd3d73e89b2d21
MotacillaAlba <rowena.rowena.lily@gmail.com>
2022-11-21 01:35:46 +0800

    finished ppo

--- algorithms/ppo.py
+++ algorithms/ppo.py
@@ -104,7 +104,7 @@ class PPOConfig(ConfigBase):
         self.clip_epsilon = 0.2
 
         ## actor net
-        self.lr_actor = self.lr
+        self.lr_actor = 3e-4
         self.actor_net = PPOActor
         self.actor_kwargs = {
             "discrete": self.discrete,
@@ -114,7 +114,7 @@ class PPOConfig(ConfigBase):
         }
 
         ## critic net
-        self.lr_critic = self.lr
+        self.lr_critic = 4e-4
         self.critic_net = PPOCritic
         self.critic_kwargs = {
             "state_dim": self.state_dim,
@@ -162,6 +162,7 @@ class PPO(AgentBase):
 
         batches = self.buffer.all()
         state = torch.FloatTensor(batches["state"]).to(device)
+        action = torch.FloatTensor(batches["action"]).to(device)
         reward = torch.FloatTensor(batches["reward"]).unsqueeze(-1).to(device)
         done = torch.FloatTensor(batches["done"]).unsqueeze(-1).to(device)
         next_state = torch.FloatTensor(batches["next_state"]).to(device)
@@ -172,9 +173,8 @@ class PPO(AgentBase):
         advantage = []
         with torch.no_grad():
             value =  self.critic_net(state)
-            next_value = self.critic_net(next_state)
-        deltas = reward + self.configs.gamma * done * next_value - value
-
+            value_next = self.critic_net(next_state)
+        deltas = reward + self.configs.gamma * done * value_next - value
         for delta, is_done in zip(reversed(deltas.detach().cpu().numpy()), reversed(done.detach().cpu().numpy())):
             gae = delta + self.configs.gamma * is_done * self.configs.gae_lambda * gae
             advantage.append(gae)
@@ -184,7 +184,15 @@ class PPO(AgentBase):
 
         for _ in range(self.configs.epoch):
             for indices in BatchSampler(SubsetRandomSampler(range(self.configs.horizon-1)), self.configs.batch_size, True):
-                _, log_prob = self.actor_net(state[indices])
+                if self.configs.discrete:
+                    probs, _ = self.forward(state)
+                    probs = self.softmax(probs)
+                    dist = Categorical(probs)
+                else:
+                    mean, log_std = self.actor_net(state[indices])
+                    std = torch.exp(log_std)
+                    dist = Normal(mean, std)
+                log_prob = dist.log_prob(action[indices])
                 ratio = torch.exp(log_prob - old_log_prob[indices])
 
                 loss_cpi = ratio * advantage[indices]
@@ -195,7 +203,7 @@ class PPO(AgentBase):
                 actor_loss.backward()
                 self.actor_optimizer.step()
 
-                critic_loss = F.mse_loss(self.critic_net(state[indices]), v_target[indices])
+                critic_loss = F.mse_loss(self.critic_net(state[indices]), v_target[indices]).mean()
                 self.critic_optimizer.zero_grad()
                 critic_loss.backward()
                 self.critic_optimizer.step()
